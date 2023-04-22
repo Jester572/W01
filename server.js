2b19a83ac885807bd4904cd359e83631fedd4eae
Jester572 <jester572@yahoo.com>
2023-04-22 02:41:02 -0600

    trying to understand git

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Jazlynn Earley");
+    res.send("Evelynn Earley");
 });
 
 const port = 3000
