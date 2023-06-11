0910371cbf627f7c2c8d3aed7e229dfa213044d0
Jester572 <jester572@yahoo.com>
2023-06-10 22:22:40 -0600

    :)

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -1,13 +1,13 @@
 const jesseRoute = (req, res) => {
-    res.send("Jesse Earley");
+    res.send("Jesse Earley!");
 };
 
 const jazlynnRoute = (req, res) => {
-    res.send("Jazlynn Earley");
+    res.send("Jazlynn Earley!");
 };
 
 const evelynnRoute = (req, res) => {
-    res.send("Evelynn Earley");
+    res.send("Evelynn Earley!");
 };
 
 module.exports = {
