1f1016ab778369b744ead2f3c807992382e7dcbd
Jester572 <jester572@yahoo.com>
2023-06-10 22:23:41 -0600

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1')
 
+//LocalHost:3000/
 routes.get('/', lesson1Controller.jesseRoute);
 routes.get('/jazlynn', lesson1Controller.jazlynnRoute);
 routes.get('/evelynn', lesson1Controller.evelynnRoute);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Server file
 const express = require('express');
 const app = express();
 
