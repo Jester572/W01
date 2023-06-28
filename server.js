3b03b94ec15e0e8f0d6095830b3457be8cac1d43
Jester572 <jester572@yahoo.com>
2023-06-28 02:05:42 -0600

    added debug

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //Server file
 const express = require('express');
 const connectDB = require('./db/connect');
+const bodyParser = require('body-parser');
 const app = express();
 
 
 connectDB;
 const port = 3000
 
+app.use(bodyParser.json())
 app.use('/', require('./routes/contacts'));
 
 app.listen(process.env.port || port);
