1b9d161eff9253774f377857ecabd142982ae27d
Jester572 <jester572@yahoo.com>
2023-07-06 17:22:23 -0600

    Revert "Finished Ui"
    This reverts commit 93592feab7dddf19e57abedc323d4a71dfd0d29a.

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const routes = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json');
-const contactsController = require('../controllers/contacts');
 
-routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const contactsController = require('../controllers/contacts');
 
 routes.get('/', contactsController.getContactList);
 
