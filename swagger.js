daae0ddfc443e602d0b3b3e9ca06f8cac8e56fd0
Jester572 <jester572@yahoo.com>
2023-07-06 18:09:48 -0600

    :((

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Description'
     },
-    host: 'https://cse341-lesson-1-e5z6.onrender.com',
+    host: 'cse341-lesson-1-e5z6.onrender.com',
     basePath: '/',
     schemes: ['https']
 };
